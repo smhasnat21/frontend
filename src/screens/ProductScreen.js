5e14a1f836a4d06d3f20ea4211f5f7df86c70160
S M Hasnat Hossain <smhasnat21@gmail.com>
2022-05-31 23:11:29 +0600

    Add to Cart

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,10 +1,19 @@
 import React from 'react'
-import {useParams,Link } from "react-router-dom";
-import { Row, Col, Image, ListGroup,Container } from 'react-bootstrap'
+import { useParams, Link } from 'react-router-dom'
+import {
+  Row,
+  Col,
+  Image,
+  ListGroup,
+  Container,
+  Card,
+  Button,
+} from 'react-bootstrap'
+import Rating from '../components/Rating'
 import products from '../products'
 
 const ProductScreen = () => {
-const id = useParams()
+  const id = useParams()
   const product = products.find((p) => p._id === id?.id)
 
   return (
@@ -22,6 +31,45 @@ const id = useParams()
               <h3>{product.name}</h3>
             </ListGroup.Item>
           </ListGroup>
+          <ListGroup.Item>
+            <Rating
+              value={product.rating}
+              text={`${product.numReviews} reviews`}
+            />
+          </ListGroup.Item>
+          <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+          <ListGroup.Item>Description: {product.description}</ListGroup.Item>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
         </Col>
       </Row>
     </Container>
